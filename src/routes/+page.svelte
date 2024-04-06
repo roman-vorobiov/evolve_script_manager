8e6e94794c8858af5b240096bd18333b12f93b3a
romanv <roman.vorobyov7777@gmail.com>
2024-04-06 17:27:58 +0200

    ui setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+
+    function importConfig() {
+        console.log("hello");
+    }
+</script>
+
+<svelte:head>
+    <title>Evolve Script Manager</title>
+</svelte:head>
+
+<Button variant="outline" class="text-button" on:click={importConfig}>Import</Button>
