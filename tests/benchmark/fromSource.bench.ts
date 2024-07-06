3ba3d4b9f4ecc449af6385066e85331cade58e3d
romanv <roman.vorobyov7777@gmail.com>
2024-07-06 19:53:35 +0200

    benchmark each step

--- tests/benchmark/fromSource.bench.ts
+++ tests/benchmark/fromSource.bench.ts
@@ -3,13 +3,63 @@ import exampleConfig from "$lib/assets/example.txt?raw";
 import { describe, bench } from "vitest";
 
 import { fromSource } from "$lib/core/dsl";
+import { parseSource } from "$lib/core/dsl/parser/parser";
 
-const model = {
-    main: exampleConfig
-};
+import { inlineReferences }        from "$lib/core/dsl/compiler/inlineReferences";
+import { resolveWildcards }        from "$lib/core/dsl/compiler/wildcards";
+import { resolveFolds }            from "$lib/core/dsl/compiler/folds";
+import { resolvePlaceholders }     from "$lib/core/dsl/compiler/placeholders";
+import { resolvePrefixes }         from "$lib/core/dsl/compiler/prefixes";
+import { resolveAliases }          from "$lib/core/dsl/compiler/aliases";
+import { validateTypes }           from "$lib/core/dsl/compiler/validation";
+import { applyConditionBlocks }    from "$lib/core/dsl/compiler/conditionBlocks";
+import { collectLogFilterStrings } from "$lib/core/dsl/compiler/logFilter";
+import { collectIgnoredTechs }     from "$lib/core/dsl/compiler/ignoredResearch";
+import { buildEvolutionQueue }     from "$lib/core/dsl/compiler/evolutionQueue";
+import { flattenExpressions }      from "$lib/core/dsl/compiler/conditions";
+import { createTriggerChains }     from "$lib/core/dsl/compiler/triggers";
+import { createTriggerConditions } from "$lib/core/dsl/compiler/triggerConditions";
+import { normalizeStatements }     from "$lib/core/dsl/compiler/normalize";
 
-describe("Compilation", () => {
+const steps = [
+    inlineReferences,
+    resolveWildcards,
+    resolveFolds,
+    resolvePlaceholders,
+    resolvePrefixes,
+    resolveAliases,
+    validateTypes,
+    applyConditionBlocks,
+    collectLogFilterStrings,
+    collectIgnoredTechs,
+    buildEvolutionQueue,
+    flattenExpressions,
+    createTriggerChains,
+    createTriggerConditions,
+    normalizeStatements,
+];
+
+const { nodes, sourceMap } = parseSource({ main: exampleConfig }, "main");
+
+const statements: any[] = [nodes];
+for (const step of steps) {
+    statements.push(step(statements.at(-1), sourceMap, [], []));
+}
+
+describe("Steps", () => {
+    bench("parse", () => {
+        parseSource({ main: exampleConfig }, "main");
+    });
+
+    for (let i = 0; i != steps.length; ++i) {
+        bench(steps[i].name, () => {
+            steps[i](statements[i], sourceMap, [], [])
+        });
+    }
+});
+
+describe("Full", () => {
     bench("fromSource", () => {
-        fromSource(model, "main");
+        fromSource({ main: exampleConfig }, "main");
     });
 });
