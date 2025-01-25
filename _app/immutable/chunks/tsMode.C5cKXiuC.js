1fa0aee96916f3d327bd94346afbedfe7c609c30
romanv <roman.vorobyov7777@gmail.com>
2025-01-25 14:06:43 +0100

    more settings

--- _app/immutable/chunks/tsMode.C5cKXiuC.js
+++ _app/immutable/chunks/tsMode.C5cKXiuC.js
@@ -1,4 +1,4 @@
-import{m as I}from"./2.CGZ2xOzu.js";import{t as N}from"./editor.main.BYpXR5_N.js";/*!-----------------------------------------------------------------------------
+import{m as I}from"./2.bd8VXR5U.js";import{t as N}from"./editor.main.a7m8ZgMo.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.47.0(69991d66135e4a1fc1cf0b1ac4ad25d429866a0d)
  * Released under the MIT license
